b8f9ee35849e07af866a2fd7f5042c42e2b08c79
Mariam <mariebukhaidze@gmail.com>
2025-09-25 10:45:30 +0400

    added in imports dessertsPageCommponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DessertsPageComponent } from './page/desserts-page/desserts-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DessertsPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'desserts-order-app';
