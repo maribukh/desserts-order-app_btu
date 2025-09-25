eb737b1e3c003b5713c1298473bee2ef748f1c4f
Ruska-N <ninidze.rusudan@gmail.com>
2025-09-26 03:58:54 +0400

    empty cart created

--- src/app/page/desserts-page/desserts-page.component.ts
+++ src/app/page/desserts-page/desserts-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { DessertListComponent } from '../../components/dessert-list/dessert-list.component';
+import { CartComponent } from '../../cart/cart.component';
 
 @Component({
   selector: 'app-desserts-page',
-  imports: [DessertListComponent],
   templateUrl: './desserts-page.component.html',
-  styleUrl: './desserts-page.component.css',
+  styleUrls: ['./desserts-page.component.css'],
+  standalone: true,
+  imports: [DessertListComponent, CartComponent]
 })
 export class DessertsPageComponent {}
+
